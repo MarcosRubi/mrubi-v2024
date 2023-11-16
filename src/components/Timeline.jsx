1266f6976bf5e2b363d6be9c5113790fb33b571e
MarcosRubi <danielhernandez9980@gmail.com>
2023-11-16 03:33:12 -0600

    Timeline horizontal usando splidejs

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,22 +1,43 @@
-import { Chrono } from 'react-chrono'
+import React from 'react'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css'
 import data from '../data/timeline.json'
+
 function Timeline () {
+  const options = {
+    type: 'loop',
+    focus: 'center',
+    autoHeight: true
+  }
   return (
     <section className='timeline'>
       <div className='container'>
         <h2 className='text-center'>Mi proceso</h2>
-        <Chrono
-          items={data.map(item => item)}
-          mode='VERTICAL_ALTERNATING'
-          hideControls
-          activeItemIndex={0}
-          useReadMore={false}
-          theme={{ primary: 'var(--color-link)', secondary: 'var(--color-link)', cardBgColor: 'var(--bg-footer)', titleColor: 'var(--text-color)', titleColorActive: 'var(--bg-color)' }}
-          buttonTexts={{ first: 'Ir al principio', last: 'Ir al final', next: 'Siguiente', previous: 'Anterior' }}
-        />
-
+        <Splide aria-label='Mi proceso de aprendizaje' options={options}>
+          {
+            data.map((timeline, id) => {
+              return (
+                <SplideSlide key={id}>
+                  <div className='flex items-center flex-column'>
+                    <div className='img'>
+                      <img src={timeline.url} alt='Image 1' />
+                    </div>
+                    <div className='content'>
+                      <span className=' block'>{timeline.date}</span>
+                      <h3>{timeline.cardTitle}</h3>
+                      {timeline.cardDetailedText.map((text, index) => (
+                        <p key={index}>{text}</p>
+                      ))}
+                    </div>
+                  </div>
+                </SplideSlide>
+              )
+            })
+          }
+        </Splide>
       </div>
     </section>
   )
 }
+
 export default Timeline
