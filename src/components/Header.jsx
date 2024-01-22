6a2a1e7ff9a714069769d68a17fcd18d13c0cc4c
MarcosRubi <danielhernandez9980@gmail.com>
2024-01-22 08:08:49 -0600

    Cambiando Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ function Header () {
     <header className={`menu fixed w-100 ${sticky}`}>
       <div className='container flex align-center justify-between relative'>
         <a href='/' className='logo' aria-label='Ir a la página de inicio'>
-          <img src='/logo.svg' alt='Logo de Marcos Rubí - Gema Ruby' loading='eager' width='40px' height='40px' />
+          <img src='/logo.webp' alt='Logo de Marcos Rubí - Gema Ruby' loading='eager' width='40px' height='40px' />
         </a>
         <Nav menuStyle={menuStyle} setMenuStyle={setMenuStyle} />
         <div className='flex align-center'>
