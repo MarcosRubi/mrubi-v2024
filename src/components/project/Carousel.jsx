0a567ed298199253a9b060ba6a39b01417452f59
MarcosRubi <danielhernandez9980@gmail.com>
2024-05-07 19:24:43 -0600

    Actualizando informacion y proyectos

--- src/components/project/Carousel.jsx
+++ src/components/project/Carousel.jsx
@@ -6,9 +6,6 @@ export const ArrowRight = () => {
   return <svg width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='feather feather-arrow-right'><line x1='5' y1='12' x2='19' y2='12' /><polyline points='12 5 19 12 12 19' /></svg>
 }
 function ImageSlider ({ project }) {
-  const sizes = [
-    'Móvil', 'Tablet', 'Escritorio'
-  ]
   const images = project.filter((project) => project.id !== 1)
   const [wordData, setWordData] = useState(images[0])
   const [val, setVal] = useState(0)
@@ -33,13 +30,13 @@ function ImageSlider ({ project }) {
 
   return (
     <div className='carousel-project flex flex-column align-center img'>
-      <span className='size-img'>Diseño en {sizes[val]} </span>
+      <span className='size-img'>Diseño en {images[val].size} </span>
       <div className='carousel-main-image flex '>
         <button className='btn' onClick={handlePrevious}><ArrowLeft /></button>
         <img src={wordData.url} height='500' width='725' className='main-image-carousel' />
         <button className='btn ' onClick={handleNext}><ArrowRight /></button>
       </div>
-      <div className='carousel-images flex justify-center flex-wrap'>
+      <div className='carousel-images flex justify-center flex-wrap max-w-md'>
         {images.map((image, i) =>
           <div className='thumbnail' key={i}>
             <img className={val === i ? 'clicked' : ''} src={image.url} onClick={() => handleClick(i)} height='70' width='100' />
